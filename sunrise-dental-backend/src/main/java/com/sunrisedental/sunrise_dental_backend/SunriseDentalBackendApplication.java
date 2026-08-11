ae5a171e6eaf04502f274ab034c10051f916b603
Banuka <156705400+Banuka-M@users.noreply.github.com>
2026-08-12 02:57:08 +0530

    Initial Spring Boot project

---
+++ sunrise-dental-backend/src/main/java/com/sunrisedental/sunrise_dental_backend/SunriseDentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunrisedental.sunrise_dental_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseDentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseDentalBackendApplication.class, args);
+	}
+
+}
---
+++ sunrise-dental-backend/src/test/java/com/sunrisedental/sunrise_dental_backend/SunriseDentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunrisedental.sunrise_dental_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseDentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
